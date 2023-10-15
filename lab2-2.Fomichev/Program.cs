2d7be43dd9f04d02bb7d17ac34a9d714a34a50ef
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-10-15 19:45:59 +0200

    Добавьте файлы проекта.

---
+++ lab2-2.Fomichev/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координату x:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y:");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в четвертой четверти");
+}
+else
+{
+    Console.WriteLine("Точка не находится в четвертой четверти.");
+}
